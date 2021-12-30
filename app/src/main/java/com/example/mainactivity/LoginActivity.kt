22f2c1373eef5aacc8d9fc04bf6f5d0b35a45784
rayanwil <rayan.wilmart@student.hogent.be>
2021-12-30 18:19:15 +0100

    inloggen + registreren
    done

--- app/src/main/java/com/example/mainactivity/LoginActivity.kt
+++ app/src/main/java/com/example/mainactivity/LoginActivity.kt
@@ -1,13 +1,42 @@
 package com.example.mainactivity
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity :AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        
 
+
+
+        buttonInloggen.setOnClickListener {
+           inloggen()
+        }
+
+    }
+
+    private fun inloggen (){
+        val email=emailLogin.text.toString()
+        val password=passwordLogin.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this,"Gelieve alle velden in te vullen",Toast.LENGTH_SHORT).show()
+            return
+        }
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if(!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("Main","User ingelogd: ${it.result.user?.uid}")
+            }
+            .addOnFailureListener{
+                Toast.makeText(this,"Niet gelukt om in te loggen :${it.message} ",
+                    Toast.LENGTH_SHORT).show()
+            }
     }
 }
--- app/src/main/java/com/example/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/mainactivity/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,17 +14,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         buttonRegister.setOnClickListener {
-            val email=emailRegister.text.toString()
-            val password=passwordRegister.text.toString()
-
-            Log.d("MainActivity","email is"+email)
-            Log.d("MainActivity","password is"+password)
+           Register()
         }
 
         alreadyAccounttextView.setOnClickListener {
-            Log.d("MainActivity","try to show login")
-            val intent=Intent(this,LoginActivity::class.java)
-            startActivity(intent)
+           goToLogin()
         }
+    }
+
+
+    private fun goToLogin(){
+        Log.d("MainActivity","try to show login")
+        val intent=Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+    }
+    private fun Register(){
+        val email=emailRegister.text.toString()
+        val password=passwordRegister.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this,"Gelieve alle velden in te vullen",Toast.LENGTH_SHORT).show()
+            return
+        }
+        Log.d("MainActivity","email is"+email)
+        Log.d("MainActivity","password is"+password)
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if(!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("Main","User created: ${it.result.user?.uid}")
+
+            }
+            .addOnFailureListener {
+                Log.d("Main","Niet gelukt om gebruiker aan te maken :${it.message} ")
+                Toast.makeText(this,"Niet gelukt om gebruiker aan te maken :${it.message} ",Toast.LENGTH_SHORT).show()
+            }
     }
 }
