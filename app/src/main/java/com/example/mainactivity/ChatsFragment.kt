625e3c59dcf21612694de5ab5b22a365026422d8
rayanwil <rayan.wilmart@student.hogent.be>
2022-01-03 02:02:47 +0100

    chatfragment updated with dummy data

--- app/src/main/java/com/example/mainactivity/ChatsFragment.kt
+++ app/src/main/java/com/example/mainactivity/ChatsFragment.kt
@@ -5,21 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
 
 class ChatsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var recyclerView: RecyclerView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false)
+       val view: View = inflater.inflate(R.layout.fragment_chats, container, false)
+
+
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpDummyRows()
+    }
+
+    private fun setUpDummyRows() {
+        val adapter=GroupAdapter<ViewHolder>()
+
+
+        adapter.add(LatestMessageRow())
+        adapter.add(LatestMessageRow())
+        adapter.add(LatestMessageRow())
+
+        recyclerView= view?.findViewById(R.id.recyclerview_latest_messages)
+        recyclerView!!.adapter=adapter
+    }
+
+
+}
+
+class LatestMessageRow: Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.latest_messages_row
+    }
 
 }
