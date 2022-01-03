f785ebc32ec177a02af3b0125a96a6af7e3b6770
rayanwil <rayan.wilmart@student.hogent.be>
2022-01-03 13:23:40 +0100

    chat fully working

--- app/src/main/java/com/example/mainactivity/ChatsFragment.kt
+++ app/src/main/java/com/example/mainactivity/ChatsFragment.kt
@@ -1,21 +1,25 @@
 package com.example.mainactivity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mainactivity.models.ChatMessage
+import com.example.mainactivity.models.User
+import com.example.mainactivity.views.LatestMessageRow
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.chat_from_row.view.*
 import kotlinx.android.synthetic.main.latest_messages_row.view.*
 
 class ChatsFragment : Fragment() {
@@ -29,13 +33,30 @@ class ChatsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
        val view: View = inflater.inflate(R.layout.fragment_chats, container, false)
-        recyclerView= view?.findViewById(R.id.recyclerview_latest_messages)
-        recyclerView!!.adapter=adapter
-        listenForLatestMessages()
+        //recyclerView= view?.findViewById(R.id.recyclerview_latest_messages)
+       // val textview :TextView=view?.findViewById(R.id.textViewLatestMessage)
+        //recyclerView!!.adapter=adapter
+        //recyclerView!!.addItemDecoration(DividerItemDecoration(textview.context,DividerItemDecoration.VERTICAL))
+        //listenForLatestMessages()
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView= view?.findViewById(R.id.recyclerview_latest_messages)
+        //val textview :TextView=view?.findViewById(R.id.textViewUsernameLatestMessage)
+        recyclerView!!.adapter=adapter
+        adapter.setOnItemClickListener { item, view ->
+            val intent=Intent(view.context,ChatLogActivity::class.java)
 
+            val row= item as LatestMessageRow
+
+            intent.putExtra(SearchFragment.USER_KEY,row.partnerUser)
+            startActivity(intent)
+        }
+        //recyclerView!!.addItemDecoration(DividerItemDecoration(textview.context,DividerItemDecoration.VERTICAL))
+        listenForLatestMessages()
+    }
     private fun listenForLatestMessages() {
         val fromId = FirebaseAuth.getInstance().uid
         val refUsers= FirebaseDatabase.getInstance("https://chatappcustomandroid-default-rtdb.europe-west1.firebasedatabase.app/").getReference("/latest-messages/${fromId}")
@@ -46,40 +67,29 @@ class ChatsFragment : Fragment() {
                 if(message != null){
                     latestMessagesMap[p0.key!!]=message
                     refreshRecyclerViewMessages()
-
-                  // adapter.add(LatestMessageRow(message))
-
                 }
 
             }
-
             override fun onChildChanged(p0: DataSnapshot, previousChildName: String?) {
                 val message= p0.getValue(ChatMessage::class.java)
 
                 if(message != null){
                     latestMessagesMap[p0.key!!]=message
                     refreshRecyclerViewMessages()
-
-                    // adapter.add(LatestMessageRow(message))
-
                 }
             }
-
             override fun onChildRemoved(snapshot: DataSnapshot) {
 
             }
-
             override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
 
             }
-
             override fun onCancelled(error: DatabaseError) {
 
             }
 
         })
     }
-
     private fun refreshRecyclerViewMessages() {
         adapter.clear()
         latestMessagesMap.values.forEach{
@@ -87,16 +97,5 @@ class ChatsFragment : Fragment() {
         }
     }
 
-    class LatestMessageRow(val chatMessage: ChatMessage): Item<ViewHolder>(){
-        override fun bind(viewHolder: ViewHolder, position: Int) {
-
-            viewHolder.itemView.textViewLatestMessage.text=chatMessage.text
-        }
-
-        override fun getLayout(): Int {
-            return R.layout.latest_messages_row
-        }
-
-    }
 }
 
