c1036b016f640b17efb92342ac23620e65fc1e1f
rayanwil <rayan.wilmart@student.hogent.be>
2022-01-05 14:35:33 +0100

    retrofit added + api calls

--- app/src/main/java/com/example/mainactivity/newsapi/NewsApi.kt
+++ app/src/main/java/com/example/mainactivity/newsapi/NewsApi.kt
@@ -1,4 +1,32 @@
 package com.example.mainactivity.newsapi
 
+import com.example.mainactivity.jsonclasses.NewsResponse
+import com.example.mainactivity.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NewsApi {
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode:String="be",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String= API_KEY
+    ): Response<NewsResponse>
+
+
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery:String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String= API_KEY
+    ): Response<NewsResponse>
 }
---
+++ app/src/main/java/com/example/mainactivity/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.mainactivity.util
+
+class Constants {
+    companion object{
+        const val API_KEY="6d72b9c2d2f14b2e9ad8197058d415e5"
+        const val BASE_URL="https://newsapi.org"
+    }
+}
