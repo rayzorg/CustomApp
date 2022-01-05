f9e80bd0385bd33c097657608f9a4823e0e60086
rayanwil <rayan.wilmart@student.hogent.be>
2022-01-05 14:48:58 +0100

    Create ArticleDao.kt

---
+++ app/src/main/java/com/example/mainactivity/db/ArticleDao.kt
@@ -0,0 +1,17 @@
+package com.example.mainactivity.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.mainactivity.jsonclasses.Article
+
+@Dao
+interface ArticleDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles():LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
