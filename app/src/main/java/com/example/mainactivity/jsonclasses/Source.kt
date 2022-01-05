40fde563f3545a294b7a00c0326a37d66a28c4ef
rayanwil <rayan.wilmart@student.hogent.be>
2022-01-05 14:16:05 +0100

    classes added + interface

---
+++ app/src/main/java/com/example/mainactivity/jsonclasses/Source.kt
@@ -0,0 +1,6 @@
+package com.example.mainactivity.jsonclasses
+
+data class Source(
+    val id: Any,
+    val name: String
+)
---
+++ app/src/main/java/com/example/mainactivity/newsapi/NewsApi.kt
@@ -0,0 +1,4 @@
+package com.example.mainactivity.newsapi
+
+interface NewsApi {
+}
