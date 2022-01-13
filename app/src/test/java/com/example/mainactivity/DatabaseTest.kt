3a29851096c1fca9691b528510093966a9b7710c
rayanwil <rayan.wilmart@student.hogent.be>
2022-01-13 20:42:43 +0100

    Update DatabaseTest.kt

--- app/src/test/java/com/example/mainactivity/DatabaseTest.kt
+++ app/src/test/java/com/example/mainactivity/DatabaseTest.kt
@@ -1,7 +1,6 @@
 package com.example.mainactivity
 
 import android.content.Context
-import androidx.lifecycle.Observer
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -9,32 +8,22 @@ import com.example.mainactivity.db.ArticleDao
 import com.example.mainactivity.db.ArticleDatabase
 import com.example.mainactivity.models.Article
 import com.example.mainactivity.models.Source
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.hamcrest.CoreMatchers.*
 import org.junit.runner.RunWith
 import java.io.IOException
 import org.junit.Assert.*
-import androidx.lifecycle.LifecycleOwner
 
-import androidx.lifecycle.LifecycleRegistry
-import com.example.mainactivity.repository.NewsRepository
-import org.mockito.Mock
 
 
 @RunWith(AndroidJUnit4::class)
 class DatabaseTest {
-
     private lateinit var articleDao: ArticleDao
     private lateinit var db: ArticleDatabase
-     private lateinit var repo: NewsRepository
-
     @Before
-    fun testcreateDb() {
+    fun createDb() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(
             context, ArticleDatabase::class.java
@@ -69,7 +58,6 @@ class DatabaseTest {
         val one= articleDao.getArticles(1)
 
         assertEquals(one.size,0)
-
     }
     @Test
     @Throws(Exception::class)
@@ -80,6 +68,7 @@ class DatabaseTest {
 
         articleDao.upsert(article)
         articleDao.upsert(article2)
+
         val one= articleDao.getArticles(2)
 
         assertEquals(one.size,2)
