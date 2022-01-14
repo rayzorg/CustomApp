78bdd2521b96b1331ba8a5a5f153e9afaa3eb424
rayanwil <rayan.wilmart@student.hogent.be>
2022-01-14 20:14:38 +0100

    extra test functions

--- app/src/androidTest/java/com/example/mainactivity/MessagesActivityTest.kt
+++ app/src/androidTest/java/com/example/mainactivity/MessagesActivityTest.kt
@@ -1,16 +1,22 @@
 package com.example.mainactivity
 
+import android.content.res.Resources
+import android.view.Menu
+import androidx.test.annotation.UiThreadTest
+import androidx.test.filters.SmallTest
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.example.mainactivity.models.User
 import com.google.android.gms.tasks.Task
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -19,7 +25,9 @@ import org.junit.runners.JUnit4
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
-import java.lang.Exception
+import kotlin.collections.HashMap
+import kotlin.collections.Map
+import kotlin.collections.set
 
 @RunWith(JUnit4::class)
 class MessagesActivityTest {
@@ -32,6 +40,12 @@ class MessagesActivityTest {
     private lateinit var authResultTask: Task<AuthResult>
     @Mock
     private lateinit var authResultTaskLogin: Task<AuthResult>
+    private val menuContentItemIds = intArrayOf(
+        R.id.breakingNewsFragment , R.id.savedNewsFragment, R.id.searchNewsFragment
+    )
+    private var menuStringContent: Map<Int, String>? = null
+
+    private var bottomNavigation: BottomNavigationView? = null
 
     @get:Rule
     val mActivityTestRule: ActivityTestRule<MessagesActivity> = ActivityTestRule(
@@ -41,6 +55,17 @@ class MessagesActivityTest {
     @Throws(Exception::class)
     fun setUp() {
         messagesActivity = mActivityTestRule.activity
+        val activity: MessagesActivity = mActivityTestRule.activity
+        activity.setTheme(R.style.Theme_MaterialComponents_Light)
+        bottomNavigation = activity.findViewById(R.id.bottomNavigationViewChat)
+        val res: Resources = activity.resources
+        menuStringContent = HashMap(menuContentItemIds.size)
+        (menuStringContent as HashMap<Int, String>)[R.id.chatsFragment2] =
+            res.getString(R.string.chats)
+        (menuStringContent as HashMap<Int, String>)[R.id.searchFragment4] =
+            res.getString(R.string.zoeken)
+        (menuStringContent as HashMap<Int, String>)[R.id.searchNewsFragment] =
+            res.getString(R.string.settings)
     }
     @Test
     fun testUserLogin() {
@@ -73,5 +98,36 @@ class MessagesActivityTest {
                 }
             })
         }
+    }
+
+    @UiThreadTest
+    @Test
+    @SmallTest
+    fun testAddItemsWithoutMenuInflation() {
+        val navigation = BottomNavigationView(mActivityTestRule.activity)
+        //mActivityTestRule.activity.setContentView(navigation)
+        navigation.menu.add("Item1")
+        navigation.menu.add("Item2")
+        assertEquals(2, navigation.menu.size())
+        navigation.menu.removeItem(0)
+        navigation.menu.removeItem(0)
+        assertEquals(0, navigation.menu.size())
+    }
+
+    @Test
+    @SmallTest
+    fun testBasics() {
+        // Check the contents of the Menu object
+        val menu: Menu = bottomNavigation!!.menu
+        assertNotNull("Menu should not be null", menu)
+        assertEquals(
+            "Should have matching number of items",
+            menuContentItemIds.size,
+            menu.size()
+        )
+       /* for ((index,value) in MENU_CONTENT_ITEM_IDS.withIndex() ) {
+            val currItem: MenuItem = menu[index]
+            assertEquals("ID for Item #$index", index, value)
+        }*/
     }
 }
--- app/src/androidTest/java/com/example/mainactivity/newsui/NewsActivityTest.kt
+++ app/src/androidTest/java/com/example/mainactivity/newsui/NewsActivityTest.kt
@@ -1,11 +1,19 @@
 package com.example.mainactivity.newsui
 
+import android.content.res.Resources
+import android.view.Menu
+import androidx.test.annotation.UiThreadTest
+import androidx.test.filters.SmallTest
 import androidx.test.rule.ActivityTestRule
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import java.lang.Exception
+
 
 @RunWith(JUnit4::class)
 class NewsActivityTest {
@@ -14,9 +22,57 @@ class NewsActivityTest {
     val mActivityTestRule: ActivityTestRule<NewsActivity> = ActivityTestRule(
         NewsActivity::class.java
     )
+    private val menuContentItemIds = intArrayOf(
+       com.example.mainactivity.R.id.breakingNewsFragment , com.example.mainactivity.R.id.savedNewsFragment, com.example.mainactivity.R.id.searchNewsFragment
+    )
+    private var menuStringContent: Map<Int, String>? = null
+
+    private var bottomNavigation: BottomNavigationView? = null
+
+
     @Before
     @Throws(Exception::class)
     fun setUp() {
         newsActivity = mActivityTestRule.activity
+        val activity: NewsActivity = mActivityTestRule.activity
+        activity.setTheme(com.example.mainactivity.R.style.Theme_MaterialComponents_Light)
+        bottomNavigation = activity.findViewById(com.example.mainactivity.R.id.bottomNavigationView)
+        val res: Resources = activity.resources
+        menuStringContent = HashMap(menuContentItemIds.size)
+        (menuStringContent as HashMap<Int, String>)[com.example.mainactivity.R.id.breakingNewsFragment] =
+            res.getString(com.example.mainactivity.R.string.breaking_news)
+        (menuStringContent as HashMap<Int, String>)[com.example.mainactivity.R.id.savedNewsFragment] =
+            res.getString(com.example.mainactivity.R.string.opgeslagen)
+        (menuStringContent as HashMap<Int, String>)[com.example.mainactivity.R.id.searchNewsFragment] = res.getString(com.example.mainactivity.R.string.zoeken)
+    }
+
+    @UiThreadTest
+    @Test
+    @SmallTest
+    fun testAddItemsWithoutMenuInflation() {
+        val navigation = BottomNavigationView(mActivityTestRule.activity)
+       // mActivityTestRule.activity.setContentView(navigation)
+        navigation.menu.add("Item1")
+        navigation.menu.add("Item2")
+        assertEquals(2, navigation.menu.size())
+        navigation.menu.removeItem(0)
+        navigation.menu.removeItem(0)
+        assertEquals(0, navigation.menu.size())
+    }
+    @Test
+    @SmallTest
+    fun testBasics() {
+        // Check the contents of the Menu object
+        val menu: Menu = bottomNavigation!!.menu
+        Assert.assertNotNull("Menu should not be null", menu)
+        assertEquals(
+            "Should have matching number of items",
+            menuContentItemIds.size,
+            menu.size()
+        )
+        /* for ((index,value) in MENU_CONTENT_ITEM_IDS.withIndex() ) {
+             val currItem: MenuItem = menu[index]
+             assertEquals("ID for Item #$index", index, value)
+         }*/
     }
 }
